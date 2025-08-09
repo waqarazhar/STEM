ee228309be5211c51c87a4622faf8e8017b2933a
waqarazhar <waqarazhar@gmail.com>
2025-08-09 21:55:51 +0100

    env setup

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,13 @@
-python3 -m venv pistem
+ENV=pistem
 
-source /home/rpi/STEM/pistem/bin/activate
+python3 -m venv $HOME/$ENV
+
+source $HOME/$ENV/bin/activate
+
+pip3 install hcsr04sensor
 
 pip install raspberrypi-tm1637
 
+pip3 install adafruit-circuitpython-ads1x15
 
+pip3 install adafruit-circuitpython-bme280
